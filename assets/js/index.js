a1a3026a0441c082058f2370f4525d1e950912af
lconteville <70072309+lconteville@users.noreply.github.com>
2021-04-08 00:36:23 -0300

    editted poster section, created geor square pic, fixed speakers text for mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -135,13 +135,13 @@ function renderCodeCon() {
 const speakersList = [
     { "name": "Daniela Robles", 'img': 'Daniela_Robles_square.png', "filiation": "UNAM - Mexico" },
     { "name": "Janet Gonzalez", "img": "Janneth_Gonzalez.png", "filiation": "PUJ - Colombia" },
-    { "name": "Sara Cuadros Orellana", 'img': 'Sara_Cuadros.JPG', "filiation": "UCM-Chile" },
+    { "name": "Sara Cuadros Orellana", 'img': 'Sara_Cuadros.JPG', "filiation": "UCM - Chile" },
     { "name": "Patricia Acosta Vargas", 'img': 'Patricia Acosta.jpg', "filiation": "UDLA - Chile" },
-    { "name": "Daiana Mir", 'img': 'daiana_mir.jpg', "filiation": "UGB-CENUR-UdelaR-Uruguay" },
-    { "name": "Maria Luisa Serrano García", 'img': 'Maria_Luisa_Serrano_squared.jpg', "filiation": "Fac. de Farmacia-UCV Venezuela" },
+    { "name": "Daiana Mir", 'img': 'daiana_mir.jpg', "filiation": "UGB - CENUR - UdelaR - Uruguay" },
+    { "name": "Maria Luisa Serrano García", 'img': 'Maria_Luisa_Serrano_squared.jpg', "filiation": "Fac. de Farmacia - UCV Venezuela" },
     { "name": "Gabriela de Queiroz", 'img': 'Gabriela_Queiroz_squared.jpg', "filiation": "IBM - USA" },
-    { "name": "Georgina Stegmayer", 'img': 'GeorginaStegmayer.png', "filiation": "UNL - CONICET - Argentine" },
-    { "name": "Han Sun", 'img': 'Han_sun_squared.jpeg', "filiation": "Leibniz-Forschungsinstitut - Germany" }
+    { "name": "Georgina Stegmayer", 'img': 'GeorginaStegmayer_square.png', "filiation": "UNL - CONICET - Argentine" },
+    { "name": "Han Sun", 'img': 'Han_sun_squared.jpeg', "filiation": "Leibniz - Forschungsinstitut - Germany" }
 ];
 
 function renderSpeakersBios() {
@@ -178,7 +178,7 @@ function renderSponsorshipSection() {
     women and leading dissidents in the fields of IT and
     academic research</p>
     <p> We offer a variety of sponsorship opportunities, including advertisement space in our conference program and website.
-    For information on sponsorship opportunities, please download the sponsorship <a target=”_blank” href="https://drive.google.com/file/d/1myA94IjMknfAJIAqnQTTakabAooh5f7z/view?usp=sharing">brochure</a>
+    For information on sponsorship opportunities, please download the sponsorship <a target=”_blank” href="https://drive.google.com/file/d/1myA94IjMknfAJIAqnQTTakabAooh5f7z/view?usp=sharing">brochure.</a>
     If you need more information on sponsorship, please email:</p> <p>womenbioinformaticsnetworkla@gmail.com</p>
     </div>`);
 
@@ -189,14 +189,14 @@ function renderRegistrationSection() {
     const registrationContent = $(`
     <h2>Registration</h2>
     <p><strong>Latin American Congress of Women in Bioinformatics and Data Science</strong> is a free event open to the entire community, which brings together professionals, students, academics, businesswomen, public officials and hobbyists. The main purpose of the meeting is to promote and make visible the research carried out by women and dissidents in the field of Bioinformatics and Data Science.</p>
-    <p>This congress focuses on the areas of systems biology, omics technologies, artificial intelligence, machine learning, data science, data mining and high performance computing with biological applications in public and private sectors</p>
+    <p>This congress focuses on the areas of systems biology, omics technologies, artificial intelligence, machine learning, data science, data mining and high performance computing with biological applications in public and private sectors.</p>
     <hr class="separator">
-    <p>Access to the registration form <a target="_blank" href="https://forms.gle/AKFMaoqqZ1ghvXN58"> here</a></p>
+    <p>Access the registration form <a target="_blank" href="https://forms.gle/AKFMaoqqZ1ghvXN58"> here</a></p>
     <p>Submit your abstract <a target="_blank" href="https://easychair.org/conferences/?conf=2ndwbdsla" >here</a></p>
     <hr class="separator">
     
     <h3>Volunteers</h3>
-    <p>If you want to join the volunteers team for helping us during the event, pleas clomplete this <a target="_blank" href="#">form</a> and soon you will be contacted. Thank you!!</p>
+    <p>If you want to join the volunteers' team to help us during the event, please complete this <a target="_blank" href="#">form</a> and soon you will be contacted. Thank you!!</p>
     `);
 
     registrationContent.appendTo(registrationSection);
@@ -207,25 +207,30 @@ function renderPosterSessionSection() {
     const posterSessionContent = $(`
     <article>
         <h2>Poster Submission</h2>
-        <p>Submitting a poster comprises sending a file in png format of the poster in the indicated format and a pre-record 3-5 minutes presentation explaining the poster.</p>
+        <p>After receiving the notification that your abstract has been accepted for poster presentation, you will be able to submit your poster. Submitting a poster comprises on sending a file in png format of the poster and a pre-recorded presentation of 3-5 minutes explaining the poster.</p>
     </article>
     <article>
-        <h2>Poster Instructions</h2>
-        <p>Participants must create a 45 cm x 53 cm high-resolution image with all the poster information in English, using the background image <a target="_blank" href="#"> provided </a> by the organizing committee. The poster should be in png format.  Please send a copy of the poster to the conference official email account, using the Subject: Virtual Poster Session. The file name should be named with the last name of the first author and the poster number:
-            “Velez_Rueda_Poster5.png”. Please send it before the 10th of September. To clearly explain your work to a broad technical audience, we recommend the following content for the image: Title, author list, affiliation, and
-            the abstract: background, or motivation to your approach, method or techniques, results, conclusions, and any related references (Please see the poster bellow as example).
+        <h2>Poster and Video Instructions</h2>
+        <p>Please send it before the 10th of September to the official email account: <a href="#" target="_blank">womenbioinformaticsnetworkla@gmail.com</a></p>
+        <h3>Poster</h3>
+        <p>Participants must create a 45 cm x 53 cm high-resolution image with all the poster information in English, using the background image <a target="_blank" href="#">provided</a> by the organizing committee. The poster should be in png format.  Please send the poster to the conference official email account, using the Subject: Virtual Poster Session. The file name should be named with the last name of the first author and the EasyChair submission number:
+            “Velez_Rueda_Poster5.png”. To clearly explain your work to a broad technical audience, we recommend the following content for the image: Title, author list, affiliation, and
+            the abstract: background, or motivation to your approach, method or techniques, results, conclusions, and any related references.
         </p>
+        <h3>Video</h3>
+        <p>It is mandatory that authors also send the pre-record of their poster talk (3-5 minutes), which will be uploaded on our <a target="_blank" href="https://www.youtube.com/watch?v=anq8YaRMQDo&feature=youtu.be">Youtube Channel</a> and will be presented on our website along with the poster. Save your presentation as an MP4 file. Please send the video to the conference official email account, using the Subject: Virtual Poster Session. The file name should be named with the last name of the first author and the EasyChair submission number: “Velez_Rueda_Poster5.mp4”.
+        </p>
+        <h3>Twitter</h3>
         <p>
-        Participants should post their posters on Twitter at the beginning of the conference using a single tweet, which should include the official poster image, with a brief explanation. In order to increase their visibility and associate them with the conference, please use the hashtags <strong>#VirtualPostersSession</strong> and </strong>#2WBDS_LA<strong>. 
-        Please use the handle <a target="_blank" href="https://twitter.com/BioinfoLa">@BioinfoLa</a> as well so that we will receive a notification. Other suggestions for hashtags are #bioinformatics #datascience #WomenSTEM or the topics of the congress <strong>#DeepLearning #DataMining&BigDataAnalysis #Chemoinformatics&DrugDiscovery #Genomics&Evolution #SystemsBiology #StructuralBioinformatics #BiomolecularSimulations #Bioinformatics&Diseases #Education</strong>
-        </p>
-        <p>If the poster is associated with a preprint/publication, we advise you to post a second tweet with its url. </p>
-        <p>In case you are in a country that does not allow for Twitter posting, If that is the case, please send it to the official email account, before the 10th of September.</p>
+        Participants can also post their posters on Twitter at the beginning of the conference using a single tweet with the poster image and a brief explanation. In order to increase the visibility and associate it with the conference, please use the hashtags <strong>#VirtualPostersSession</strong> and <strong>#2WBDS_LA</strong>. 
+        Please use the handle <a target="_blank" href="https://twitter.com/BioinfoLa">@BioinfoLa</a> as well so that we will receive a notification. Other suggestions for hashtags are #bioinformatics <strong>#datascience #WomenSTEM</strong> or the topics of the congress <strong>#DeepLearning #DataMining&BigDataAnalysis #Chemoinformatics&DrugDiscovery #Genomics&Evolution #SystemsBiology #StructuralBioinformatics #BiomolecularSimulations #Bioinformatics&Diseases #Education</strong>. 
+        If the poster is associated with a preprint/publication, we advise you to post a second tweet with its url.
+        In case you are in a country that does not allow Twitter posting, please send it to the official email account before the 10th of September and we will post it for you.</p>
 
-        <h2>Virtual Poster session</h2>
-        <p>We will hold a virtual poster session that will take place on Twitter and our website. It will start one week before the conference (22 to 24th September 2020) and will continue until the end of the conference. All official posters will be retweeted by <a target="_blank" href="https://twitter.com/BioinfoLa">@BioinfoLa</a>. Official email account: <a href="#" target="_blank">womenbioinformaticsnetworkla@gmail.com</a>
-        You should also send a pre-record 3-5 minute presentation which will be uploaded to our <a target="_blank" href="https://www.youtube.com/watch?v=anq8YaRMQDo&feature=youtu.be">youtube channel</a>. If that is the case, please send it to the official email account. Please send it before the 10th of September . 
-        The prerecorded poster videos will be transmitted on a virtual poster session and Open MIC track, with space for questions on live. You should also be around to reply to any comments that may arise.
+        <h2>Virtual Poster Session</h2>
+        <p>We will hold a Virtual Poster Session that will take place on this website and also on Twitter. 
+        It will start one week before the conference (22nd to 24th September 2021) and will continue until the end of the conference. 
+        All official posters will be retweeted by <a target="_blank" href="https://twitter.com/BioinfoLa">@BioinfoLa</a>. 
         </p>
  
         <h2>FREQUENTLY ASKED QUESTIONS</h2>
@@ -238,15 +243,17 @@ function renderPosterSessionSection() {
         <h3>Who is the corresponding author?</h3>
         <p>Correspondence will be sent to the author indicated as corresponding at the time of submission.</p>
         <h3>Can I send only the poster?</h3>
-        <p>No. Poster submission requires a poster and a video</p>
+        <p>No. Poster submission requires a poster and a video.</p>
         <h3>Can I send only the video?</h3>
-        <p>No, Poster submission requires a poster and a video</p>
-        <h3>Which topics does congress have?</h3>
-        <p>Deep Learning </p>
-        <p>Data mining and big data analysis, Chemoinformatics and Drug discovery</p>
-        <p>Genomics and Evolution, Systems Biology</p>
-        <p>Structural Bioinformatics - Biomolecular Simulations </p>
-        <p>Bioinformatics and Diseases, Education</p>
+        <p>No, Poster submission requires a poster and a video.</p>
+        <h3>Which topics does the congress have?</h3>
+        <ul>
+            <li>Bioinformatics and Diseases</li>
+            <li>Biomolecular Simulations, Structural Bioinformatics & Systems Biology</li>
+            <li>Chemoinformatics and Drug Discovery</li>
+            <li>Genomics and Evolution</li>
+            <li>Machine Learning, Data mining and Big Data Analysis</li>
+        </ul>
     </article>
     `);
 
@@ -266,7 +273,7 @@ function renderCommitteesSection() {
     <ul>
         <li>Ana Julia Velez Rueda, Universidad Nacional de Quilmes - CONICET, Argentine</li>
         <li>Lucy Jiménez, IT - Machine Learning - Mercado Libre, Colombia</li>
-        <li>Liliane Conteville, IOC, Brazil</li>
+        <li>Liliane Conteville, IOC - Fiocruz, Brazil</li>
         <li>Ivana Feldfeber, Observatorio de Datos con Perspectiva de Género, Argentine</li>
         <li>Agustina Nardo, CIDCA- CONICET, Argentine</li>
         <li>Mercedes Didier Garnham, Trypanosomatics Lab, IIB-UNSAM, Argentine</li>
@@ -282,22 +289,22 @@ function renderPortuguese() {
     sponsorsButton.text('Patrocinadores');
     committeeButton.text('Organizadoras');
     h1About.innerHTML = "2ª Conferência de Mulheres em Bioinformática e Ciência de Dados LA";
-    pAbout.innerHTML = "O Congresso Latino-Americano de Mulheres em Bioinformática e Ciência de Dados é um evento gratuito aberto a toda a comunidade, que reúne profissionais, estudantes, acadêmicas, empresárias, funcionários públicos e amadores. O principal objetivo do encontro é divulgar e dar visibilidade à pesquisa desenvolvida por mulheres e dissidentes da área de Bioinformática e Ciência de Dados. Este congresso foca nas áreas de biologia de sistemas, tecnologias ômicas, inteligência artificial, aprendizado de máquina, ciência de dados, mineração de dados e computação de alto desempenho com aplicações biológicas nos setores público e privado.";
+    pAbout.innerHTML = "O Congresso Latino-Americano de Mulheres em Bioinformática e Ciência de Dados é um evento gratuito aberto a toda a comunidade, que reúne profissionais, estudantes, acadêmicas, empresárias, funcionárias públicas e amadoras. O principal objetivo do encontro é divulgar e dar visibilidade à pesquisa desenvolvida por mulheres e dissidentes da área de Bioinformática e Ciência de Dados. Este congresso foca nas áreas de biologia de sistemas, tecnologias ômicas, inteligência artificial, aprendizado de máquina, ciência de dados, mineração de dados e computação de alto desempenho com aplicações biológicas nos setores público e privado.";
     ConductText.innerHTML = "Código de Conduta";
     h2Title.innerHTML = "CONGRESSO VIRTUAL GRATUITO";
-    h3Title.innerHTML = "de 22 a 24 de setembro de 2021";
+    h3Title.innerHTML = "de 22 a 24 de SETEMBRO de 2021";
     workshopButton.text('Cursos');
     datesbutton.text('Datas Importantes');
     posterButton.text('Sessão de Pôsteres');
     h2dates.innerHTML = "Datas Importantes";
-    thdates.innerHTML = "Envio de resumos para palestras e pôsteres";
+    thdates.innerHTML = "Envio de Resumos para Palestras e Pôsteres";
     open1.innerHTML = "Abertura da Chamada para Resumo";
     open2.innerHTML = "15 de abril de 2021";
     close1.innerHTML = "Encerramento da Chamada para Resumo";
     close2.innerHTML = "1º de julho de 2021";
     notify1.innerHTML = "Notificação de Aceitação de Pôster";
     notify2.innerHTML = "30 de julho de 2021";
-    h2Sponsors.innerHTML = "Apoie este evento";
+    h2Sponsors.innerHTML = "Apoiam este evento";
     h4Contact.innerHTML = "Contate-nos por e-mail:";
     h4Media.innerHTML = "Nos siga nas redes sociais:";
 }
@@ -308,20 +315,20 @@ function renderSpanish() {
     sponsorsButton.text('Patrocinadores');
     committeeButton.text('Organizadoras');
     h1About.innerHTML = "2do Congreso de Mujeres en Bioinformática y Ciencia de Datos LA";
-    pAbout.innerHTML = "El Congreso Latinoamericano de Mujeres en Bioinformática y Ciencia de Datos es un evento gratuito abierto a toda la comunidad, que reúne a profesionales, estudiantes, académicas, empresarias, funcionarias y amateurs. El objetivo principal del encuentro es Dar a conocer y dar visibilidad a la investigación que realizan mujeres y disidentes en el área de Bioinformática y Ciencia de Datos. Este congreso se centra en las áreas de biología de sistemas, tecnologías ómicas, inteligencia artificial, aprendizaje automático, ciencia de datos, minería de datos y Rendimiento informático de alto nivel con aplicaciones biológicas en el sector público y privado. ";
+    pAbout.innerHTML = "El Congreso Latinoamericano de Mujeres en Bioinformática y Ciencia de Datos es un evento gratuito abierto a toda la comunidad, que reúne a profesionales, estudiantes, académicas, empresárias, funcionarias y amateurs. El objetivo principal del encuentro es dar a conocer y dar visibilidad a la investigación que realizan mujeres y disidentes en el área de Bioinformática y Ciencia de Datos. Este congreso se centra en las áreas de biología de sistemas, tecnologías ómicas, inteligencia artificial, aprendizaje automático, ciencia de datos, minería de datos y Rendimiento informático de alto nivel con aplicaciones biológicas en el sector público y privado. ";
     ConductText.innerHTML = "Código de conducta";
     h2Title.innerHTML = "CONGRESO VIRTUAL GRATIS";
-    h3Title.innerHTML = "22-24 de septiembre de 2021";
+    h3Title.innerHTML = "22-24 de SEPTIEMBRE de 2021";
     workshopButton.text('Cursos');
     datesbutton.text('Fechas importantes');
     posterButton.text('Sesión de Póster');
     h2dates.innerHTML = "Fechas importantes";
     thdates.innerHTML = "Envío de resúmenes para conferencias y carteles";
-    open1.innerHTML = "Apertura de la convocatoria de resumen";
+    open1.innerHTML = "Apertura de la Convocatoria de Resumen";
     open2.innerHTML = "15 de abril de 2021";
-    close1.innerHTML = "Convocatoria de cierre para resumen";
+    close1.innerHTML = "Convocatoria de Cierre para Resumen";
     close2.innerHTML = "1 de julio de 2021";
-    notify1.innerHTML = "Notificación de aceptación de póster";
+    notify1.innerHTML = "Notificación de Aceptación de Póster";
     notify2.innerHTML = "30 de julio de 2021";
     h2Sponsors.innerHTML = "Apoya este evento";
     h4Contact.innerHTML = "Contáctenos por correo electrónico:";
